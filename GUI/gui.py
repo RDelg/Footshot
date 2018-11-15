69797400a829dd3059cc8bb0a9cb0a27a399cebd
Rene Delgado <rene854@gmail.com>
2018-11-15 20:25:48 -0300

    Format

--- GUI/gui.py
+++ GUI/gui.py
@@ -1,26 +1,23 @@
-import PySimpleGUI27 as sg
-import numpy as np
 import cv2
+import numpy as np
+
 import uvclite
-from time import sleep
+import PySimpleGUI27 as sg
 
 
 class Camera(object):
 
-  _frame_format = uvclite.libuvc.uvc_frame_format
+  _Y16 = _frame_format = uvclite.libuvc.uvc_frame_format.UVC_FRAME_FORMAT_Y16
 
-
-  def __init__(self, context, vendor_id=0, product_id=0, frame_format=_frame_format.UVC_FRAME_FORMAT_Y16, width=160, height=120, frame_rate=9):
+  def __init__(self, context, vendor_id=0, product_id=0, frame_format=self._Y16, width=160, height=120, frame_rate=9):
     self.context = context
     self.find_device(vendor_id, product_id)
     self.device.open()
     self.set_stream_format(frame_format, width, height, frame_rate)
     self.is_streaming = False
 
-
   def find_device(self, vendor_id, product_id):
     self.device = self.context.find_device(vendor_id, product_id)
-
 
   def set_stream_format(self, frame_format, width, height, frame_rate):
     self.frame_format = frame_format
@@ -29,43 +26,38 @@ class Camera(object):
     self.frame_rate = frame_rate
     self.device.set_stream_format(frame_format, width, height, frame_rate)
 
-  
   def start_streaming(self):
     if not self.is_streaming:
       self.device.start_streaming()
       self.is_streaming = True
 
-  
   def stop_streaming(self):
     if self.is_streaming:
       self.device.stop_streaming()
       self.start_streaming = False
 
-  
   def get_frame(self, timeout):
-    assert self.is_streaming , "La camara no esta en streaming"
+    assert self.is_streaming, "La camara no esta en streaming"
     return self.device.get_frame(timeout)
 
-
-  def get_img_raw(self, timeout, size = (320, 240)):
+  def get_img_raw(self, timeout, size=(320, 240)):
     frame = self.get_frame(timeout)
     if frame.size != (2*self.height*self.width):
       return None
-    data = np.frombuffer(frame.data, dtype=np.uint16).reshape(self.height, self.width)
-    if self.width != size[0] or self.height!= size[1]: 
+    data = np.frombuffer(frame.data, dtype=np.uint16).reshape(
+        self.height, self.width)
+    if self.width != size[0] or self.height != size[1]:
       data = cv2.resize(data, size)
     cv2.normalize(data, data, 0, 65535, cv2.NORM_MINMAX)
     np.right_shift(data, 8, data)
     img = cv2.cvtColor(np.uint8(data), cv2.COLOR_GRAY2RGB)
     return cv2.imencode('.png', img)[1].tobytes()
 
-
-  def get_img_jpg(self, timeout, size = (320, 240)):
+  def get_img_jpg(self, timeout, size=(320, 240)):
     frame = self.get_frame(timeout)
     data = np.frombuffer(frame.data, dtype=np.uint16)
     img = cv2.imdecode(data, 1)
     return cv2.imencode('.png', img)[1].tobytes()
-
 
   def __del__(self):
     if self.streaming:
@@ -76,35 +68,40 @@ class Camera(object):
 
 class FootGui(object):
 
-  _left_frame = [      
-                  [sg.T('Text inside of a frame')],      
-                  [sg.CB('Check 1'), sg.CB('Check 2')],
-                  [sg.Image(data='', background_color='gray', size=(320, 240), key='infrarrojo', tooltip ='Imagen IR')]      
-                ]
-  _right_frame =  [      
-                    [sg.T('Text inside of a frame')],      
-                    [sg.CB('Check 1'), sg.CB('Check 2')],
-                    [sg.Image(data='', background_color='green', size=(320, 240), key='visual', tooltip ='Imagen Visual')]      
-                  ]
+  _left_frame = [
+      [sg.T('Text inside of a frame')],
+      [sg.CB('Check 1'), sg.CB('Check 2')],
+      [sg.Image(data='', background_color='gray', size=(
+          320, 240), key='infrarrojo', tooltip='Imagen IR')]
+  ]
+  _right_frame = [
+      [sg.T('Text inside of a frame')],
+      [sg.CB('Check 1'), sg.CB('Check 2')],
+      [sg.Image(data='', background_color='green', size=(
+          320, 240), key='visual', tooltip='Imagen Visual')]
+  ]
   _layout = [
-              [
-                sg.Frame('Infrarrojo', _left_frame, font='Any 12', title_color='blue'),
-                sg.Frame('Visible', _right_frame, font='Any 12', title_color='blue')
-              ],
-              [
-                sg.T(' '  * 10),
-                sg.Button('Record', size=(5, 1), font='Helvetica 14'),
-                sg.Button('Stop', size=(5, 1), font='Any 14'),
-                sg.Button('Exit', size=(5, 1), font='Helvetica 14'),
-                sg.Button('About', size=(5,1), font='Any 14')
-              ],
-              [
-                sg.T(' '  * 10)
-              ]
-            ]
+      [
+          sg.Frame('Infrarrojo', _left_frame,
+                   font='Any 12', title_color='blue'),
+          sg.Frame('Visible', _right_frame,
+                   font='Any 12', title_color='blue')
+      ],
+      [
+          sg.T(' ' * 10),
+          sg.Button('Record', size=(5, 1), font='Helvetica 14'),
+          sg.Button('Stop', size=(5, 1), font='Any 14'),
+          sg.Button('Exit', size=(5, 1), font='Helvetica 14'),
+          sg.Button('About', size=(5, 1), font='Any 14')
+      ],
+      [
+          sg.T(' ' * 10)
+      ]
+  ]
 
   def __init__(self):
-    self.window = sg.Window('Footshot', grab_anywhere=True, icon= 'Isotipo.ico').Layout(self._layout)
+    self.window = sg.Window('Footshot', grab_anywhere=True,
+                            icon='Isotipo.ico').Layout(self._layout)
 
   def read(self, timeout):
     return self.window.Read(timeout)
@@ -135,13 +132,13 @@ def main():
         img_vis = cam_VIS.get_img_jpg(0)
         fg.update_img(img_ir, img_vis)
         if event == 'Exit':
-          loop = false
+          loop = False
       except uvclite.UVCError as e:
-            print(e)
-            if(e[1] == 110):
-                continue
-            else:
-                break
+        print(e)
+        if(e[1] == 110):
+          continue
+        else:
+          break
 
 
 if __name__ == '__main__':
